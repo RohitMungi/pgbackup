1e283d8213bf314b902af581e94a37cb81d3d695
Rohit.Mungi <rohitmungi@gmail.com>
2018-11-03 15:06:06 +0000

    Adding changes before building a distribution

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     long_description=readme,
     author='Rohit',
     author_email='rohitmungi@gmail.com',
-    install_requires=[],
+    install_requires=['boto3'],
     packages=find_packages('src'),
-    package_dir={'':'src'}
+    package_dir={'':'src'},
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ]
+    }
 )
--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -14,10 +14,30 @@ class DriverAction(Action):
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('url', help = "URL of the postgresql database to backup")
-    parser.add_argument('--driver',
+    parser.add_argument('--driver', '-d',
             help="how & where to store the backup",
             nargs=2,
             action=DriverAction,
+            metavar = ('driver','destination'),
             required=True
             )
     return parser
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump,storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S",time.localtime())
+        file_name = pgdump.dump_file_name(args.url,timestamp)
+        storage.s3(client,dump.stdout,args.destination,file_name)
+        print(f"Backing database to {args.destination} in S3 as {file_name}")
+    else:
+        outfile = open(args.destination,'wb')
+        print(f"Backing database up to locally to S3 as {args.file_name}")
+
+        storage.local(dump.stdout,outfile)
